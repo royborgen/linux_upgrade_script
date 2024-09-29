21670994f8b00d8a6eec1ef4b4a8beba946957f7
Roy Borgen <60260940+royborgen@users.noreply.github.com>
2024-09-29 23:55:45 +0200

    Update upgrade.sh
    added coloring to text and a test to see if reboot is required to apply updates

--- upgrade.sh
+++ upgrade.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-echo "Checking APT:"
-sudo apt update && sudo apt upgrade -y 
+CYAN='\033[0;36m'
+YELLOW='\033[1;33m'
+NOCOLOR='\033[0m'
+echo "${CYAN}Checking APT:${NOCOLOR}"
+sudo apt update && sudo apt upgrade -y
 echo ""
-echo "Checking Snap:"
-sudo snap refresh 
+echo "${CYAN}Checking Snap:${NOCOLOR}"
+sudo snap refresh
 echo ""
-echo "Checking Flatpak:"
+echo "${CYAN}Checking Flatpak:${NOCOLOR}"
 flatpak upgrade --noninteractive
+echo ""
+test -e /var/run/reboot-required && echo "${YELLOW}Reboot is required!${NOCOLOR}"
